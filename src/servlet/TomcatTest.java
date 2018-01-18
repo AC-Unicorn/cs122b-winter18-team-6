9863d92c8e9f0b3315b07d89cd56e08f6f1f1263
AC-Unicorn <a894657305@qq.com>
2018-01-17 18:24:22 -0800

    css style update ver 0.0.1

--- src/servlet/TomcatTest.java
+++ src/servlet/TomcatTest.java
@@ -43,15 +43,62 @@ public class TomcatTest extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         out.println("<HTML><HEAD><TITLE>Movie List</TITLE></HEAD>");
-        out.println("<style> body{background:url('https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/03/13/inception.jpg'); padding:0;margin:0;} #nav{height:20px;width100%;margin:0;background-color:yellow; } #banner{ height:120px;width:100%;background:url('http://www.publicdomainpictures.net/pictures/80000/velka/logo-banner.jpg');margin:0; }  ");
-        out.println("ul{display:block;background-color:white;border:1px solid;border-radius:5%;opacity:0.5;}");
-       
+        out.println("<style>");
+//        out.println("<style> body{background:url('https://tctechcrunch2011.files.wordpress.com/2015/08/safe_image.gif'); padding:0;margin:0;} #nav{height:20px;width100%;margin:0;background-color:yellow; } #banner{ height:120px;width:100%;background:url('https://tctechcrunch2011.files.wordpress.com/2015/08/safe_image.gif');margin:0; }  ");
+////        out.println("ul{display:block;background-color:white;border:1px solid;border-radius:5%;opacity:0.5;} li{list-style-type:none;}");
+////       
+////        out.println("@keyframes Rainbox_text{0%{color:red;} 25%{color:blue;} 50%{color:green;} 100%{color:yellow;}}");
+////        out.println(".movie_box{animation:Rainbox_text ; animation-duration:2s; animation-iteration-count:infinite;}");
+//        
         
+        out.println("body{font-family:times,serif;background:url('http://www.mlabc.com/uploads/Texture/0004/000c/%E7%81%B0%E8%89%B2%E6%9C%A8%E7%BA%B9_000180_h280.jpg'); padding:0;margin:0px;} #nav{ height:60px;width100%;margin-bottom:0;text-align:center; } #banner{ height:140px;background-color:white;margin-top:0;padding:0;item-align:center; }  \r\n" + 
+        		".movie_box ul{display:block;background-color:white;margin:0;} li{list-style-type:none;}\r\n" + 
+        		"\r\n" + 
+        		"\r\n" + 
+        		"#top20{\r\n" + 
+        		"margin-left:46.8%;\r\n" + 
+        		"}\r\n" + 
+        		"\r\n" + 
+        		"#nav ul{\r\n" + 
+        		"display:inline-block;\r\n" + 
+        		"padding:0;\r\n" + 
+        		"margin:0;\r\n" + 
+        		"margin-left:20px;\r\n" + 
+        		"}\r\n" + 
+        		"\r\n" + 
+        		"#nav li{\r\n" + 
+        		"margin-top:15px;\r\n" + 
+        		"margin-left:55px;\r\n" + 
+        		"color:white;\r\n" + 
+        		"display:block;\r\n" + 
+        		"float:left;\r\n" + 
+        		"color:white;\r\n" + 
+        		"font-size:1.4em;\r\n" + 
+        		"}\r\n" + 
+        		"\r\n" + 
+        		"\r\n" + 
+        		"#twenty{color:yellow; }\r\n" + 
+        		"\r\n" + 
+        		".movie_box{\r\n" + 
+        		"padding: 15px 20px;\r\n" + 
+        		"margin:10px 20%;\r\n" + 
+        		"text-align:center;\r\n" + 
+        		"background:url('https://i.pinimg.com/originals/9f/87/64/9f8764ef849360e7eaee2365ca8a810b.jpg');\r\n" + 
+        		"background-size:cover;\r\n" + 
+        		"color:RGB(61,76,138);\r\n" + 
+        		"opacity:0.8;\r\n" + 
+        		"}\r\n" + 
+        		"\r\n" + 
+        		".title_text{\r\n" + 
+        		"   font-size:1.5em;\r\n" + 
+        		"   color:RGB(196,62,58);\r\n" + 
+        		"   text-decoration:blink;\r\n" + 
+        		"}");
         
         
         out.println("</style>");
-        out.println("<BODY><div id='nav'> <a>Home</a> <a>Movie</a> <a>Purchase</a>    </div>");
-        out.println("<div id='banner'><h1>Fablix Top 20</h1></div>");
+        out.println("<BODY><div id='nav'> <ul><li>Home</li> <li>Movie</li> <li>Purchase</li></ul>    </div>");
+        out.println("<div id='banner'><img id='top20'src='https://www.pcc.edu/about/awards/images/top-20.png' alt='Top 20' width = 150px; >  </div>");
         try {
             //Class.forName("org.gjt.mm.mysql.Driver");
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -88,6 +135,9 @@ public class TomcatTest extends HttpServlet {
                 String m_genres = "";
                 while(rs2.next())m_genres += rs2.getString(8)+",";
                 
+                rs2.close();
+                statement2.close();
+                
                 
                 String query3 = "select * from movies,stars_in_movies,stars where movies.id='"+rs.getString(1) +"' and movies.id = movieId and starId = stars.id;";
                 
@@ -96,10 +146,16 @@ public class TomcatTest extends HttpServlet {
                 ResultSet rs3 = statement3.executeQuery(query3);
                 while(rs3.next()) m_stars += rs3.getString(8) + ",";
                 
+                rs3.close();
+                statement3.close();
+                
+                
+                
                 
                 out.println("<div class='movie_box'>");
-                out.println( "<ul> <li>Title: " + m_title + "</li>" + "<li>Year: " + m_year + "</li>" + "<li>Director: " + m_director + "</li>" + "<li>Rate: "
+                out.println( "<ul> <li><span class='title_text'>Title: " + m_title + "</span></li>" + "<li>Year: " + m_year + "</li>" + "<li>Director: " + m_director + "</li>" + "<li>Rate: "
                         +m_rating+"</li>"+"<li>genre: "+m_genres+"</li>"+"<li>Actors: " +m_stars+ "</li>" +"</ul>");
+                out.println("</div>");
             }
 
             
